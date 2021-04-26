642e494716dbf54b23b1206624a92cf0106b1bb7
Ghader Sahlehi <amirghsl1380@gmail.com>
2021-04-26 15:53:56 +0430

    style files updated

--- src/components/MenuItems/MenuButtons.js
+++ src/components/MenuItems/MenuButtons.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import Button from '@material-ui/core/Button';
+import { btn_font} from "./MenuItems.module.scss";
 
 function MenuButtons(props) {
     return (
        
-        <Button className='m-4 mb-2 p-3' 
+        <Button className={`m-4 mb-2 p-3 ` }
         style={{
             width : props.width , 
-            borderRadius: props.radius
+            borderRadius: props.radius,
+            backgroundColor:  "rgb(69, 96, 185)"
         }}  
         variant="contained" 
         color={props.color}
         onClick={() => props.event()}
 
         >
-           {props.text}
+            <span className={`${btn_font}`}>
+            {props.text}
+            </span>
+           
         </Button>
       
     )
